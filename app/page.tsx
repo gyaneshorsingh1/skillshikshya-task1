f1861207981cdc3702e16f002e7b5362f5390f81
gyaneshorsingh1 <gyaneshorsingh9.com>
2026-04-10 16:11:01 +0530

    all the codes are pushed

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,78 @@
-import Image from "next/image";
 
+
+import "./home.css"
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <>
+       <div className="container">
+          <p className="text-center note">Note: Hover the component to view the animation & Click the arrow icon</p>
+
+          <div className="main-container">
+            <div className="title-section">
+              <p className="p2">Your SkillShikshya Journey</p>
+              <p className="p3"><span>Step</span> In. <span>Skill</span> Up. <span>Stand</span> Out.🚀</p>
+            </div>
+
+            <div className="sub-container">
+                <div className="box box1">
+                  <div className="card-details">
+                    <div className="left-img"><img className="img1" src="/Group.png" alt="" /></div>
+                    <div className="content">
+                        <div className="top">
+                          <h3>Start With Clarity</h3>
+                          <p>Step into a better learning path</p>
+                        </div>
+                        <div className="bottom">
+                          <p>Overwhelmed by too many learning options? SkillShikshya provides a clear, curated roadmap from the start. Whether you're a beginner or upskilling, we have a path tailored to your growth.</p>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="box box2">
+                  <div className="card-details">
+                    <div className="right-side">
+                        <div className="top">
+                          <h3>Learn by Doing</h3>
+                          <p>Practical skills, real projects.</p>
+                        </div>
+                        <div className="bottom">
+                          <p>Theory is great, but action is better. At SkillShikshya, you learn by doing. Hands-on projects and real-world scenarios help you build, break, and create—leading to true mastery.</p>
+                        </div>
+                    </div>
+                    <div className="right-img"><img className="img2" src="/group3.png" alt="" /></div>
+                  </div>
+                </div>
+                <div className="box box3">
+                  <div className="card-details">
+                    <div className="left-img"><img className="img3" src="/Group2.png" alt="" /></div>
+                    <div className="content">
+                        <div className="top">
+                          <h3>Get Mentored & Supported</h3>
+                          <p>You're not learning alone.</p>
+                        </div>
+                        <div className="bottom">
+                          <p>Stuck or need feedback? SkillShikshya’s community of mentors and learners has your back with live support, interactive discussions, and expert insights. You’re never on your own.</p>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="box box4">
+                  <div className="card-details">
+                    <div className="right-side">
+                        <div className="top">
+                          <h3>Achieve & Showcase</h3>
+                          <p>Build your portfolio, get job-ready.</p>
+                        </div>
+                        <div className="bottom">
+                          <p>Your journey ends with achievement. Each completed project builds a portfolio showcasing your skills and job readiness, bringing you closer to that dream job, promotion, or your own venture.</p>
+                        </div>
+                    </div>
+                    <div className="right-img"><img className="img4" src="/group4.png" alt="" /></div>
+                  </div>
+                </div>
+            </div>
+          </div>
+       </div>
+    </>
   );
 }
